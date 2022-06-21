f0ed454af6c5894198d6adff92d0f35bbe0187a0
Avi Dalal <avi413@gmail.com>
2022-06-21 21:15:42 +0300

    login

--- backend/app.js
+++ backend/app.js
@@ -1,36 +1,37 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const helmet = require('helmet');
-const bodyParser = require('body-parser');
-const cards = require('./routes/cards');
-const users = require('./routes/users');
-const {login} = require('./controllers/login')
-const {createUser} = require('./controllers/users');
-const auth = require('./middlewares/auth');
+var cors = require("cors");
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+const cards = require("./routes/cards");
+const users = require("./routes/users");
+const { createUser, login } = require("./controllers/users");
+const auth = require("./middlewares/auth");
 
 const { PORT = 3000 } = process.env;
 
 mongoose
-  .connect('mongodb://0.0.0.0:27017/aroundb', {
+  .connect("mongodb://0.0.0.0:27017/aroundb", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
     const app = express();
-    app.disable('etag');
+    app.disable("etag");
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(helmet());
 
-    app.post('/signin', login);
-    app.post('/signup', createUser);
+    app.use(cors());
+    app.post("/signin", login);
+    app.post("/signup", createUser);
 
-    app.use('/users', auth, users);
-    app.use('/cards', auth, cards);
+    app.use("/users", auth, users);
+    app.use("/cards", auth, cards);
 
-    app.get('*', (req, res) => {
-      res.status(404).send({ message: 'Requested resource not found' });
+    app.get("*", (req, res) => {
+      res.status(404).send({ message: "Requested resource not found" });
     });
 
     app.listen(PORT, () => {
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,7 +1,8 @@
 const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const User = require('../models/user');
-
+const { NODE_ENV, JWT_SECRET } = process.env;
+const jwt = require('jsonwebtoken');
 
 const isValid = (err, res) => {
   if (err.name === 'ValidationError') {
@@ -9,6 +10,36 @@ const isValid = (err, res) => {
   }
   return res.status(500).send({ message: err.message });
 };
+
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+          return res.status(401).send({ message: 'Incorrect email or password' });
+
+      }
+      // user.password is the hash from the database
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+
+            if (!matched) {
+              return res.status(401).send({ message: 'Incorrect email or password' });
+            }
+            // successful authentication
+            const token = jwt.sign({ _id: user._id },NODE_ENV === 'production' ? JWT_SECRET : 'not-so-secret-string', {
+              expiresIn: '7d',
+            });
+            res.send({ token });
+
+         })
+        .catch((err) => {
+           res.status(401).send({ message: err.message });
+         });
+        })
+      }
 
 module.exports.getUsers = (req, res) => {
   User.find({})
